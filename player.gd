c8d1d102bb48c2bf3ee3bf205aa17e096c7892c4
Matteo Rosati <matteo.rosati@akqa.com>
2023-09-18 11:15:26 +0200

    add basic movement

--- player.gd
+++ player.gd
@@ -1,13 +1,30 @@
 extends Area2D
 
-@export var speed : float = 400.0
+@export var _speed : int = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	
+	## grab the input
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	## calculate speed and animate movement
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * _speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
-func _physics_process(delta):
-	pass
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)

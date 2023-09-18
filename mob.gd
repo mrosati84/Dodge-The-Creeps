b2d1d7f016742ed5b7a7264d3fd1003ba1ad3629
Matteo Rosati <matteo.rosati@akqa.com>
2023-09-18 14:28:43 +0200

    add main scene

--- mob.gd
+++ mob.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	## play the default animation
 	$AnimatedSprite2D.play("walk")
+
+## deletes the mob when exiting the screen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
